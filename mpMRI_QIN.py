82503a04ba8e98cc88decd3ea3e6f00bda3edd1d
cbri92 <70302171+cbri92@users.noreply.github.com>
2022-04-29 14:01:14 +1000

    Update mpMRI_QIN.py

--- mpMRI_QIN.py
+++ mpMRI_QIN.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Oct  1 16:13:06 2020
 
-@author: cbri3325
+@author: Caterina Brighi
 """
 
 #%% Import functions 
@@ -26,7 +26,7 @@ from ImageAnalysisFunctions import *
 
 #%% Set Working directory
         
-data_supradir = 'C:/Users/cbri3325/Dropbox (Sydney Uni)/Caterina Brighi/Data/test/' #Set working directory
+data_supradir = 'path to supradirectory containing individual patients directories' #Set working directory
 
 subjs_path = [ f.path for f in os.scandir(data_supradir) if f.is_dir() ] #Create a list of the paths to the subjects directories
 subjs_name = [ f.name for f in os.scandir(data_supradir) if f.is_dir() ] #Create a list of subjects names
